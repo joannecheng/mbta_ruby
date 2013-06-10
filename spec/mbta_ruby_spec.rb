dac10df7167d2c0b4b2225c87a94500c35bf27fa
Joanne Cheng <joanne@thoughtbot.com>
2013-06-10 07:47:00 -0600

    fix specs

--- spec/mbta_ruby_spec.rb
+++ spec/mbta_ruby_spec.rb
@@ -1,22 +1,28 @@
 require 'spec_helper'
 
 describe MBTA::Subway do
-  describe '#routes' do
+  describe '.routes' do
     it 'returns route collection' do
-      subway = MBTA::Subway.new(route_name: 'Green Line', route_id:'810_')
       MBTA::Subway.routes[0].should be_instance_of MBTA::Subway
+    end
+  end
+
+  describe '#location' do
+    it 'returns route location' do
+      subway = MBTA::Subway.new(route_name: 'Green Line', route_id:'810_')
+      subway.location
     end
   end
 end
 
 describe MBTA::CommuterRail do
-  describe '#routes'
+  describe '.routes'
 end
 
 describe MBTA::Bus do
-  describe '#routes'
+  describe '.routes'
 end
 
 describe MBTA::Boat do
-  describe '#routes'
+  describe '.routes'
 end
